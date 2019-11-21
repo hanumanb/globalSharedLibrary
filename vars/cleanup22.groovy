9f94dfff5388934309c55099aee218dba38fe416
hanumanb <hanuman.bandhu@gmail.com>
2019-11-21 14:01:04 -0500

    Update cleanup22.groovy
    Push to branch

--- vars/cleanup22.groovy
+++ vars/cleanup22.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 def call() {
-        echo "In CleanUp method asdfasdfasdf"
+        echo "In CleanUp method in Clean UP method ****"
 		echo " local jenkins execution"
        
     
