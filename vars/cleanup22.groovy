4098592d7eeb6338db670c840123f310b0ba5161
hanumanb <hanuman.bandhu@gmail.com>
2019-11-21 13:53:57 -0500

    Update cleanup22.groovy
    asdfasdf

--- vars/cleanup22.groovy
+++ vars/cleanup22.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 def call() {
-        echo "In CleanUp method - testing shared library"
+        echo "In CleanUp method asdfasdfasdf"
 		echo " local jenkins execution"
        
     
