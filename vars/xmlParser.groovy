f0db750bd0edf7820e7ed6527cc99d0612fd2860
hanumanb <hanuman.bandhu@gmail.com>
2019-11-21 15:04:21 -0500

    Update xmlParser.groovy
    Checked in the changes

--- vars/xmlParser.groovy
+++ vars/xmlParser.groovy
@@ -2,6 +2,11 @@
 def call(xmlPath) {
 echo "inside Xml parser"
 def xmlText = new XmlSlurper().parse(xmlPath)
+echo  " node name is ="  xmlText.name()
+
+xmlText.children().each {  echo it.name() }
+
+
 echo " after xml parser"
     
 }
