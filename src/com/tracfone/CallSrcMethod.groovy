e0bf6f1f25cf4bfde3e101ca4ec7b12c8c3613af
hanumanb <hanuman.bandhu@gmail.com>
2019-11-22 12:07:02 -0500

    Update CallSrcMethod.groovy
    Ok

--- src/com/tracfone/CallSrcMethod.groovy
+++ src/com/tracfone/CallSrcMethod.groovy
@@ -1,6 +1,9 @@
 package com.tracfone
 class CallSrcMethod implements Serializable {
 
+ef steps
+  CallSrcMethod(steps) {this.steps = steps}
+  
   def callSrcMethod() {
    
    echo "Inside CallSrcMethod   SRC cacll"
